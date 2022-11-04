516f1d4816d08a9ea60d097556179d8ef92b1ebe
Patryk Milkiewicz <G00397265@atu.ie>
2022-11-04 23:48:19 +0000

    Added test for the Title and Name exceptions

--- src/test/java/ie/atu/PassengerTest.java
+++ src/test/java/ie/atu/PassengerTest.java
@@ -3,6 +3,7 @@ package ie.atu;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,12 +15,24 @@ class PassengerTest {
 
     @Test
     void detailsTest(){
-        Passenger newPass = new Passenger("Mr", "Patryk", "1234567890", "0853849583", 21);
+        Passenger newPass = new Passenger("Mr", "Patryk", "1234567890", "0853849583", 26);
         assertEquals("Mr", newPass.getTitle());
         assertEquals("Patryk", newPass.getName());
         assertEquals("1234567890", newPass.getId());
         assertEquals("0853849583", newPass.getPhone());
-        assertEquals(21, newPass.getAge());
+        assertEquals(26, newPass.getAge());
+    }
+
+    @Test
+    void titleTestFail(){
+        Passenger newPass = new Passenger("TheDude", "Patryk", "1234567890", "0853849583", 21);
+        assertThrows(IllegalArgumentException.class, newPass::getTitle); //Checks if exception for wrong title is thrown
+    }
+
+    @Test
+    void nameTest(){
+        Passenger newPass = new Passenger("Mr", "Pa", "1234567890", "0853849583", 21);
+        assertThrows(IllegalArgumentException.class, newPass::getName);
     }
 
     @AfterEach
