3bc8970079becd9ab0654e26b67e14179201fbd8
Patryk Milkiewicz <G00397265@atu.ie>
2022-11-05 20:35:24 +0000

    Updated the Title test and Name test to check if the actual message was thrown.

--- src/test/java/ie/atu/PassengerTest.java
+++ src/test/java/ie/atu/PassengerTest.java
@@ -3,7 +3,6 @@ package ie.atu;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,14 +24,16 @@ class PassengerTest {
 
     @Test
     void titleTestFail(){
-        Passenger newPass = new Passenger("TheDude", "Patryk", "1234567890", "0853849583", 21);
-        assertThrows(IllegalArgumentException.class, newPass::getTitle); //Checks if exception for wrong title is thrown
+        Passenger newPass = new Passenger("King", "Patryk", "1234567890", "0853849583", 21);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, newPass::getTitle);
+        assertEquals("Please Enter a valid title", exception.getMessage());
     }
 
     @Test
-    void nameTest(){
+    void nameTestFail(){
         Passenger newPass = new Passenger("Mr", "Pa", "1234567890", "0853849583", 21);
-        assertThrows(IllegalArgumentException.class, newPass::getName);
+        IllegalArgumentException exception =  assertThrows(IllegalArgumentException.class, newPass::getName);
+        assertEquals("This is not a valid name, must be three or more characters", exception.getMessage());
     }
 
     @AfterEach
